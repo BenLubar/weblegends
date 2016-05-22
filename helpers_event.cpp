fd9233002835e8c679ba3c4a9495b4d0942bb77c
Ben Lubar <ben.lubar@gmail.com>
2016-05-21 23:21:33 -0500

    history_event_created_sitest

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -176,6 +176,40 @@ static void do_event(std::ostream & s, const event_context &, df::history_event 
     s << ENUM_KEY_STR(history_event_type, event->getType());
 }
 
+static void do_event(std::ostream & s, const event_context & context, df::history_event_created_sitest *event)
+{
+    auto site = df::world_site::find(event->site);
+
+    if (auto builder = df::historical_figure::find(event->builder_hf))
+    {
+        event_link(s, context, builder);
+        s << " founded ";
+        event_link(s, context, site);
+        if (auto civ = df::historical_entity::find(event->civ))
+        {
+            s << " in the name of ";
+            event_link(s, context, civ);
+        }
+    }
+    else if (auto civ = df::historical_entity::find(event->civ))
+    {
+        event_link(s, context, civ);
+        s << " founded ";
+        event_link(s, context, site);
+    }
+    else
+    {
+        event_link(s, context, site);
+        s << " was founded";
+    }
+
+    if (auto site_civ = df::historical_entity::find(event->site_civ))
+    {
+        s << ". The new government was named ";
+        event_link(s, context, site_civ);
+    }
+}
+
 static void do_event(std::ostream & s, const event_context & context, df::history_event_war_attacked_sitest *event)
 {
     auto attacker = df::historical_entity::find(event->attacker_civ);
