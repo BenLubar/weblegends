f120b59351d300895503fc1f17d4eb36e4193db5
Ben Lubar <ben.lubar@gmail.com>
2016-05-23 11:38:25 -0500

    entity links

--- render_entity.cpp
+++ render_entity.cpp
@@ -1,7 +1,10 @@
 #include "weblegends.h"
 #include "helpers.h"
 
+#include "df/entity_entity_link.h"
+#include "df/entity_site_link.h"
 #include "df/historical_entity.h"
+#include "df/world_site.h"
 
 void WebLegends::render_entity(std::ostream & s, int32_t id)
 {
@@ -18,6 +21,40 @@ void WebLegends::render_entity(std::ostream & s, int32_t id)
     s << "<p>";
     categorize(s, ent);
     s << "</p>";
+
+    if (!ent->entity_links.empty())
+    {
+        s << "<h2 id=\"related-entities\">Related Entities</h2><ul>";
+        for (auto it = ent->entity_links.begin(); it != ent->entity_links.end(); it++)
+        {
+            if (auto target = df::historical_entity::find((*it)->target))
+            {
+                s << "<li>";
+                link(s, target);
+                s << ",";
+                categorize(s, target);
+                s << "</li>";
+            }
+        }
+        s << "</ul>";
+    }
+    if (!ent->site_links.empty())
+    {
+        s << "<h2 id=\"related-sites\">Related Sites</h2><ul>";
+        for (auto it = ent->site_links.begin(); it != ent->site_links.end(); it++)
+        {
+            if (auto target = df::world_site::find((*it)->target))
+            {
+                s << "<li>";
+                link(s, target);
+                s << ",";
+                categorize(s, target);
+                // TODO: type
+                s << "</li>";
+            }
+        }
+        s << "</ul>";
+    }
 
     history(s, ent);
     // TODO
