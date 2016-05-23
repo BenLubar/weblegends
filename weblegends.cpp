e181c19ca787d0783aa660ef73de659f4c4ee0ed
Ben Lubar <ben.lubar@gmail.com>
2016-05-23 10:49:42 -0500

    catch the unload event before CoreSuspender is claimed

--- weblegends.cpp
+++ weblegends.cpp
@@ -10,10 +10,32 @@ DFhackCExport command_result plugin_init(color_ostream & out, std::vector<Plugin
     return weblegends->init(out);
 }
 
-DFhackCExport command_result plugin_shutdown(color_ostream & out)
+static command_result do_unload(color_ostream & out)
 {
+    if (weblegends == nullptr)
+    {
+        return CR_OK;
+    }
+
     command_result res = weblegends->shutdown(out);
     if (res == CR_OK)
+    {
         delete weblegends;
+        weblegends = nullptr;
+    }
     return res;
+}
+
+DFhackCExport command_result plugin_onstatechange(color_ostream & out, state_change_event sc)
+{
+    if (sc == SC_BEGIN_UNLOAD)
+    {
+        return do_unload(out);
+    }
+    return CR_OK;
+}
+
+DFhackCExport command_result plugin_shutdown(color_ostream & out)
+{
+    return do_unload(out);
 }
