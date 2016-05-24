93500247a85133863c0448c3dfb625bc65e497dc
Ben Lubar <ben.lubar@gmail.com>
2016-05-24 13:07:13 -0500

    CREATED_BUILDING

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -837,6 +837,60 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
     do_location_2(s, context, event);
 }
 
+static void do_event(std::ostream & s, const event_context & context, df::history_event_created_buildingst *event)
+{
+    auto site = df::world_site::find(event->site);
+    auto structure = site ? binsearch_in_vector(site->buildings, event->structure) : nullptr;
+    if (auto builder = df::historical_figure::find(event->builder_hf))
+    {
+        event_link(s, context, builder);
+        s << " built ";
+        event_link(s, context, structure);
+        if (auto site_civ = df::historical_entity::find(event->site_civ))
+        {
+            s << " for ";
+            event_link(s, context, site_civ);
+            if (auto civ = df::historical_entity::find(event->civ))
+            {
+                s << " of ";
+                event_link(s, context, civ);
+            }
+        }
+        if (auto civ = df::historical_entity::find(event->civ))
+        {
+            s << " for ";
+            event_link(s, context, civ);
+        }
+    }
+    else if (auto site_civ = df::historical_entity::find(event->site_civ))
+    {
+        event_link(s, context, site_civ);
+        if (auto civ = df::historical_entity::find(event->civ))
+        {
+            s << " of ";
+            event_link(s, context, civ);
+        }
+        s << " built ";
+        event_link(s, context, structure);
+    }
+    else if (auto civ = df::historical_entity::find(event->civ))
+    {
+        event_link(s, context, civ);
+        s << " built ";
+        event_link(s, context, structure);
+    }
+    else
+    {
+        event_link(s, context, structure);
+        s << " was built";
+    }
+    if (site)
+    {
+        s << " at ";
+        event_link(s, context, site);
+    }
+}
+
 static void do_event(std::ostream & s, const event_context & context, df::history_event_change_hf_statest *event)
 {
     std::string separator = " in ";
