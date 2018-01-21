f0a3eb18d11a6da6fb00ed8757170ff6b8d6c268
lethosor <lethosor@gmail.com>
2018-01-21 15:15:38 -0500

    Combine vertically adjacent region tiles into rectangles
    `region_coords` is sorted by x coordinates, then y coordinates. This can
    significantly cut down the size of generated SVGs in larger worlds.

--- render_region.cpp
+++ render_region.cpp
@@ -19,12 +19,7 @@ bool WebLegends::render_region(std::ostream & s, int32_t id, int32_t page)
 
     simple_header(s, region);
 
-    s << "<svg width=\"100%\" style=\"max-width:500px;border:1px solid;float:right\" viewBox=\"0 0 " << world->world_data->world_width << " " << world->world_data->world_height << "\">";
-    for (size_t i = 0; i < region->region_coords.size(); i++)
-    {
-        s << "<rect width=\"1\" height=\"1\" x=\"" << region->region_coords.x.at(i) << "\" y=\"" << region->region_coords.y.at(i) << "\"></rect>";
-    }
-    s << "</svg>";
+    render_map_coords(s, region->region_coords);
 
     s << "<p>";
     categorize(s, region);
