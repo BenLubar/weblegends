88b49dc799ce84c7f125eb1600e194ca4c7cb321
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 17:05:17 -0500

    Implement history_event_hf_confrontedst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -3167,14 +3167,45 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_hf_confrontedst *event)
 {
-    // TODO: int32_t target;
-    // TODO: int32_t accuser;
-    // TODO: std::vector<int32_t > reasons;
-    // TODO: int32_t site;
-    // TODO: int32_t region;
-    // TODO: int32_t layer;
-    // TODO: df::coord2d region_pos;
-    do_event_missing(s, context, event, __LINE__);
+    auto target = df::historical_figure::find(event->target);
+
+    event_link(s, context, target);
+
+    BEFORE_SWITCH(accuser, event->accuser);
+    switch (accuser)
+    {
+    case 0:
+        s << " aroused general suspicion ";
+        BREAK(accuser);
+    default:
+        do_event_missing(s, context, event, __LINE__);
+        break;
+    }
+    AFTER_SWITCH(accuser, stl_sprintf("event-%d (HF_CONFRONTED)", event->id));
+
+    if (!event->reasons.empty())
+    {
+        s << " after ";
+        list<int32_t>(s, event->reasons, [context, event](std::ostream & out, int32_t reason_id)
+        {
+            BEFORE_SWITCH(reason, reason_id);
+            switch (reason)
+            {
+            case 0:
+                out << "appearing not to age";
+                BREAK(reason);
+            case 1:
+                out << "a murder";
+                BREAK(reason);
+            default:
+                do_event_missing(out, context, event, __LINE__);
+                break;
+            }
+            AFTER_SWITCH(reason, stl_sprintf("event-%d (HF_CONFRONTED)", event->id));
+        });
+    }
+
+    do_location_2(s, context, event);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_entity_lawst *event)
