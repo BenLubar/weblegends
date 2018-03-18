e806eb0b4ae41af35e2ad641e2b5163ee522eb3a
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:43:57 -0500

    Implement history_event_entity_actionst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -1614,11 +1614,29 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_entity_actionst *event)
 {
-    // TODO: int32_t entity;
-    // TODO: int32_t site;
-    // TODO: int32_t structure;
-    // TODO: df::entity_action_type action;
-    do_event_missing(s, context, event, __LINE__);
+    auto entity = df::historical_entity::find(event->entity);
+    auto site = df::world_site::find(event->site);
+    auto structure = site ? binsearch_in_vector(site->buildings, event->structure) : nullptr;
+
+    event_link(s, context, entity);
+    BEFORE_SWITCH(action, event->action);
+    switch (action)
+    {
+    case entity_action_type::entity_primary_criminals:
+        s << " became the primary criminal organization in ";
+        BREAK(action);
+    case entity_action_type::entity_relocate:
+        s << " moved to ";
+        BREAK(action);
+    }
+    AFTER_SWITCH(action, stl_sprintf("event-%d (ENTITY_ACTION)", event->id));
+
+    if (structure)
+    {
+        event_link(s, context, structure);
+        s << " in ";
+    }
+    event_link(s, context, site);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_entity_incorporatedst *event)
