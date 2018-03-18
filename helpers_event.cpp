6ce7717db0e64bc56de156170e083d57bd3e9ad7
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:29:05 -0500

    Implement history_event_topicagreement_rejectedst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -1294,11 +1294,44 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_topicagreement_rejectedst *event)
 {
-    // TODO: df::meeting_topic topic;
-    // TODO: int32_t source;
-    // TODO: int32_t destination;
-    // TODO: int32_t site;
-    do_event_missing(s, context, event, __LINE__);
+    auto source = df::historical_entity::find(event->source);
+    auto destination = df::historical_entity::find(event->destination);
+    auto site = df::world_site::find(event->site);
+
+    BEFORE_SWITCH(topic, event->topic);
+    switch (topic)
+    {
+    case meeting_topic::RequestPeace:
+        s << "A peace agreement";
+        BREAK(topic);
+    case meeting_topic::TreeQuota:
+        s << "A lumber agreement";
+        BREAK(topic);
+    case meeting_topic::BecomeLandHolder:
+        s << "The establishment of landed nobility";
+        BREAK(topic);
+    case meeting_topic::PromoteLandHolder:
+        s << "The elevation of the landed nobility";
+        BREAK(topic);
+    case meeting_topic::ExportAgreement:
+    case meeting_topic::ImportAgreement:
+        s << "A trade agreement";
+        BREAK(topic);
+    case meeting_topic::TributeAgreement:
+        s << "A tribute agreement";
+        BREAK(topic);
+    default:
+        do_event_missing(s, context, event, __LINE__);
+        break;
+    }
+    AFTER_SWITCH(topic, stl_sprintf("event-%d (TOPICAGREEMENT_REJECTED)", event->id));
+
+    s << " proposed by ";
+    event_link(s, context, source);
+    s << " was rejected by ";
+    event_link(s, context, destination);
+    s << " at ";
+    event_link(s, context, site);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_topicagreement_madest *event)
