81a80997bce9b6d95b75f36287a6f367dc72efd8
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:50:28 -0500

    Implement history_event_hist_figure_reunionst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -2808,13 +2808,17 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_hist_figure_reunionst *event)
 {
-    // TODO: std::vector<int32_t > missing;
-    // TODO: std::vector<int32_t > reunited_with;
-    // TODO: int32_t assistant;
-    // TODO: int32_t site;
-    // TODO: int32_t region;
-    // TODO: int32_t layer;
-    do_event_missing(s, context, event, __LINE__);
+    list_event_link<df::historical_figure>(s, context, event->missing);
+    s << " " << (event->missing.size() == 1 ? "was" : "were") << " reunited with ";
+    list_event_link<df::historical_figure>(s, context, event->reunited_with);
+
+    if (auto assistant = df::historical_figure::find(event->assistant))
+    {
+        s << " with the help of ";
+        event_link(s, context, assistant);
+    }
+
+    do_location_2(s, context, event);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_hist_figure_reach_summitst *event)
