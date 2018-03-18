c3572ee4c0b17dadf25915522044357a18c4b98d
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 17:14:59 -0500

    Implement history_event_change_hf_moodst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -4091,15 +4091,64 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_change_hf_moodst *event)
 {
-    // TODO: int32_t histfig;
-    // TODO: df::mood_type mood;
-    // TODO: df::history_event_reason reason;
-    // TODO: int32_t site;
-    // TODO: int32_t region;
-    // TODO: int32_t layer;
-    // TODO: df::coord2d region_pos;
+    auto histfig = df::historical_figure::find(event->histfig);
 
-    do_event_missing(s, context, event, __LINE__);
+    event_link(s, context, histfig);
+
+    BEFORE_SWITCH(mood, event->mood);
+    switch (mood)
+    {
+    case mood_type::None:
+        do_event_missing(s, context, event, __LINE__);
+        BREAK(mood);
+    case mood_type::Fey:
+        s << " was taken by a fey mood";
+        BREAK(mood);
+    case mood_type::Secretive:
+        s << " withdrew from society";
+        BREAK(mood);
+    case mood_type::Possessed:
+        s << " was possessed";
+        BREAK(mood);
+    case mood_type::Macabre:
+        s << " began to skulk and brood";
+        BREAK(mood);
+    case mood_type::Fell:
+        s << " was taken by a fell mood";
+        BREAK(mood);
+    case mood_type::Melancholy:
+        s << " was striken by melancholy";
+        BREAK(mood);
+    case mood_type::Raving:
+        s << " became crazed";
+        BREAK(mood);
+    case mood_type::Berserk:
+        s << " went berserk";
+        BREAK(mood);
+    case mood_type::Baby:
+        do_event_missing(s, context, event, __LINE__);
+        BREAK(mood);
+    case mood_type::Traumatized:
+        s << " stopped responding to the outside world";
+        BREAK(mood);
+    }
+    AFTER_SWITCH(mood, stl_sprintf("event-%d (CHANGE_HF_MOOD)", event->id));
+
+    BEFORE_SWITCH(reason, event->reason);
+    switch (reason)
+    {
+    case history_event_reason::none:
+        BREAK(reason);
+    case history_event_reason::failed_mood:
+        s << " after failing to create an artifact";
+        BREAK(reason);
+    default:
+        do_event_missing(s, context, event, __LINE__);
+        break;
+    }
+    AFTER_SWITCH(reason, stl_sprintf("event-%d (CHANGE_HF_MOOD)", event->id));
+
+    do_location_2(s, context, event);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_artifact_claim_formedst *event)
