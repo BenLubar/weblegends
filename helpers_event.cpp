3bc9fc9ef2e75b63b0eb7607dcbf2aa47665015a
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:47:52 -0500

    Implement history_event_war_plundered_sitest.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -2238,11 +2238,21 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_war_plundered_sitest *event)
 {
-    // TODO: int32_t attacker_civ;
-    // TODO: int32_t defender_civ;
-    // TODO: int32_t site_civ;
-    // TODO: int32_t site;
-    do_event_missing(s, context, event, __LINE__);
+    auto attacker_civ = df::historical_entity::find(event->attacker_civ);
+    auto defender_civ = df::historical_entity::find(event->defender_civ);
+    auto site_civ = df::historical_entity::find(event->site_civ);
+    auto site = df::world_site::find(event->site);
+
+    event_link(s, context, attacker_civ);
+    s << " defeated ";
+    if (defender_civ != site_civ)
+    {
+        event_link(s, context, site_civ);
+        s << " of ";
+    }
+    event_link(s, context, defender_civ);
+    s << " and pillaged ";
+    event_link(s, context, site);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_war_site_new_leaderst *event)
