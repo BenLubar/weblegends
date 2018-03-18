788b9dc62b713334abcf186756e9f98bf9d85a07
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:49:34 -0500

    Implement history_event_war_site_taken_overst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -2329,12 +2329,24 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_war_site_taken_overst *event)
 {
-    // TODO: int32_t attacker_civ;
-    // TODO: int32_t new_site_civ;
-    // TODO: int32_t defender_civ;
-    // TODO: int32_t site_civ;
-    // TODO: int32_t site;
-    do_event_missing(s, context, event, __LINE__);
+    auto attacker_civ = df::historical_entity::find(event->attacker_civ);
+    auto new_site_civ = df::historical_entity::find(event->new_site_civ);
+    auto defender_civ = df::historical_entity::find(event->defender_civ);
+    auto site_civ = df::historical_entity::find(event->attacker_civ);
+    auto site = df::world_site::find(event->site);
+
+    event_link(s, context, attacker_civ);
+    s << " defeated ";
+    if (defender_civ != site_civ)
+    {
+        event_link(s, context, site_civ);
+        s << " of ";
+    }
+    event_link(s, context, defender_civ);
+    s << " and took over ";
+    event_link(s, context, site);
+    s << ". The new government was called ";
+    event_link(s, context, new_site_civ);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_body_abusedst *event)
