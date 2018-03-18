e8e95cf7565508f06a65ddc1b5cbcfd21dd3fb76
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 17:06:46 -0500

    Implement history_event_artifact_transformedst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -3870,12 +3870,17 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_artifact_transformedst *event)
 {
-    // TODO: int32_t new_artifact;
-    // TODO: std::vector<int32_t > old_artifact;
-    // TODO: int32_t unit;
-    // TODO: int32_t histfig;
-    // TODO: int32_t site;
-    do_event_missing(s, context, event, __LINE__);
+    auto new_artifact = df::artifact_record::find(event->new_artifact);
+    auto histfig = df::historical_figure::find(event->histfig);
+    auto site = df::world_site::find(event->site);
+
+    event_link(s, context, new_artifact);
+    s << " was made from ";
+    list_event_link<df::artifact_record>(s, context, event->old_artifact);
+    s << " in ";
+    event_link(s, context, site);
+    s << " by ";
+    event_link(s, context, histfig);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_artifact_destroyedst *event)
