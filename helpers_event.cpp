4aaba0a9e22c961f659c344d5c671df38407aa18
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 17:03:40 -0500

    Implement history_event_change_creature_typest.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -2951,13 +2951,34 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_change_creature_typest *event)
 {
-    // TODO: int32_t changee;
-    // TODO: int32_t changer;
-    // TODO: int32_t old_race;
-    // TODO: int32_t old_caste;
-    // TODO: int32_t new_race;
-    // TODO: int32_t new_caste;
-    do_event_missing(s, context, event, __LINE__);
+    auto changee = df::historical_figure::find(event->changee);
+    auto changer = df::historical_figure::find(event->changer);
+    auto old_race = df::creature_raw::find(event->old_race);
+    auto old_caste = old_race ? vector_get(old_race->caste, event->old_caste) : nullptr;
+    auto new_race = df::creature_raw::find(event->new_race);
+    auto new_caste = new_race ? vector_get(new_race->caste, event->new_caste) : nullptr;
+
+    event_link(s, context, changer);
+    s << " changed ";
+    event_link(s, context, changee);
+
+    if (old_caste)
+    {
+        s << " from a " << old_caste->caste_name[0];
+    }
+    else if (old_race)
+    {
+        s << " from a " << old_race->name[0];
+    }
+
+    if (new_caste)
+    {
+        s << " into a " << new_caste->caste_name[0];
+    }
+    else if (new_race)
+    {
+        s << " into a " << new_race->name[0];
+    }
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_hist_figure_revivedst *event)
