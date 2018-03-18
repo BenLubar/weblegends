ebda6b5f0486f0510fa3310bba710ea153b89889
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 17:03:14 -0500

    Implement history_event_create_entity_positionst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -2902,12 +2902,51 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_create_entity_positionst *event)
 {
-    // TODO: int32_t histfig;
-    // TODO: int32_t civ;
-    // TODO: int32_t site_civ;
-    // TODO: int32_t position;
-    // TODO: int16_t reason;
-    do_event_missing(s, context, event, __LINE__);
+    auto histfig = df::historical_figure::find(event->histfig);
+    auto civ = df::historical_entity::find(event->civ);
+    auto site_civ = df::historical_entity::find(event->site_civ);
+    auto position = site_civ ? binsearch_in_vector(site_civ->positions.own, event->position) : nullptr;
+
+    event_link(s, context, histfig);
+    if (civ)
+    {
+        s << " of ";
+        event_link(s, context, civ);
+    }
+
+    std::string before(" created the position of ");
+    std::string after;
+
+    BEFORE_SWITCH(reason, event->reason);
+    switch (reason)
+    {
+        case 0:
+            after = " through force of argument";
+            BREAK(reason);
+        case 1:
+            before = " compelled the creation of the position of ";
+            after = " with threats of violence";
+            BREAK(reason);
+        case 2:
+            before = " collaborated to create the position of ";
+            BREAK(reason);
+        case 3:
+            after = ", pushed by a wave of popular support";
+            BREAK(reason);
+        case 4:
+            after = " as a matter of course";
+            BREAK(reason);
+    }
+    AFTER_SWITCH(reason, stl_sprintf("event-%d (CREATE_ENTITY_POSITION)", event->id));
+
+    s << before;
+    s << (position ? html_escape(position->name[0]) : "[unknown position]");
+    if (civ != site_civ)
+    {
+        s << " of ";
+        event_link(s, context, site_civ);
+    }
+    s << after;
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_change_creature_typest *event)
