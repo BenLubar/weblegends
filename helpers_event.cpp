b11527fd585c3b4300d63488a5be9b3e1dc27b6f
Ben Lubar <ben.lubar@gmail.com>
2018-03-18 16:43:36 -0500

    Implement history_event_entity_createdst.

--- helpers_event.cpp
+++ helpers_event.cpp
@@ -1598,10 +1598,18 @@ static void do_event(std::ostream & s, const event_context & context, df::histor
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_entity_createdst *event)
 {
-    // TODO: int32_t entity;
-    // TODO: int32_t site;
-    // TODO: int32_t structure;
-    do_event_missing(s, context, event, __LINE__);
+    auto entity = df::historical_entity::find(event->entity);
+    auto site = df::world_site::find(event->site);
+    auto structure = site ? binsearch_in_vector(site->buildings, event->structure) : nullptr;
+
+    event_link(s, context, entity);
+    s << " was formed in ";
+    if (structure)
+    {
+        event_link(s, context, structure);
+        s << " in ";
+    }
+    event_link(s, context, site);
 }
 
 static void do_event(std::ostream & s, const event_context & context, df::history_event_entity_actionst *event)
