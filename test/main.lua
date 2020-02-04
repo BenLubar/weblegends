9546b72e3cbab4ce7302b52cec5ea8af8832bbb8
Ben Lubar <ben.lubar@gmail.com>
2020-02-04 03:14:53 -0600

    generate a world as part of the test suite

--- test/main.lua
+++ test/main.lua
@@ -1,3 +1,4 @@
+local gui = require('gui')
 local script = require('gui.script')
 
 function set_test_stage(stage)
@@ -9,7 +10,77 @@ end
 script.start(function()
 print('Running tests')
 
--- TODO: generate and load a small world
+print('Running file: setup')
+-- dismiss intro movie
+while dfhack.gui.getCurFocus() == 'movieplayer' do
+    gui.simulateInput(dfhack.gui.getCurViewscreen(), 'LEAVESCREEN')
+    script.sleep(1, 'frames')
+end
+
+local found = false
+local title_screen = dfhack.getCurViewscreen()
+for idx, item in ipairs(title_screen.menu_line_id) do
+    if item == df.viewscreen_titlest.T_menu_line_id.Start then
+        found = true
+        title_screen.sel_menu_line = idx
+        break
+    end
+end
+
+-- generate new world
+if not found then
+    for idx, item in ipairs(title_screen.menu_line_id) do
+        if item == df.viewscreen_titlest.T_menu_line_id.NewWorld then
+            title_screen.sel_menu_line = idx
+            gui.simulateInput(title_screen, 'SELECT')
+            script.sleep(1, 'frames')
+
+            local new_region = dfhack.getCurViewscreen()
+            while new_region.load_world_params do
+                script.sleep(1, 'frames')
+            end
+
+            if #new_region.welcome_msg > 0 then
+                gui.simulateInput(new_region, 'LEAVESCREEN')
+                script.sleep(1, 'frames')
+            end
+
+            new_region.world_size = 2
+            gui.simulateInput(new_region, 'MENU_CONFIRM')
+            while #df.global.world.entities.all == 0 and view.simple_mode == 0 and df.global.world.worldgen_status.state ~= 10 do
+                script.sleep(1, 'frames')
+            end
+            gui.simulateInput(new_region, 'SELECT')
+
+            while dfhack.gui.getCurFocus() ~= 'title' do
+                script.sleep(1, 'frames')
+            end
+
+            break
+        end
+    end
+
+    title_screen = dfhack.getCurViewscreen()
+    for idx, item in ipairs(title_screen.menu_line_id) do
+        if item == df.viewscreen_titlest.T_menu_line_id.Start then
+            title_screen.sel_menu_line = idx
+            break
+        end
+    end
+end
+print('test passed: generate-world')
+
+gui.simulateInput(title_screen, 'SELECT')
+script.sleep(1, 'frames')
+
+-- try to start in legends mode
+title_screen.sel_menu_line = 2
+gui.simulateInput(title_screen, 'SELECT')
+
+while dfhack.gui.getCurFocus() == 'adopt_region' do
+    script.sleep(1, 'frames')
+end
+print('test passed: open-world')
 
 print('Running file: main')
 local status = dfhack.run_command('weblegends-export', 'weblegends-tmp')
