6c21004e497502fe39be779576c9e882834ff7fb
Ben Lubar <ben.lubar@gmail.com>
2021-02-22 15:34:35 -0600

    implement hf_preach

---
+++ events/hf_preach.cpp
@@ -0,0 +1,26 @@
+#include "../helpers_event.h"
+
+#include "df/history_event_hf_preachst.h"
+
+void do_event(std::ostream & s, const event_context & context, df::history_event_hf_preachst *event)
+{
+    event_link(s, context, df::historical_figure::find(event->speaker_hf));
+    s << " preached to ";
+    event_link(s, context, df::historical_entity::find(event->entity1));
+    BEFORE_SWITCH(topic, event->topic);
+    switch (topic)
+    {
+        case 11:
+            s << ", inveighing against ";
+            BREAK(topic);
+        case 12:
+            s << ", urging love to be shown to ";
+            BREAK(topic);
+        default:
+            do_event_missing(s, context, event, __FILE__, __LINE__);
+            break;
+    }
+    AFTER_SWITCH(topic, stl_sprintf("event-%d (HF_PREACH)", event->id));
+    event_link(s, context, df::historical_entity::find(event->entity2));
+    do_location(s, context, event->site, -1, -1);
+}
